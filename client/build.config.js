76b2ad7f078e3342b134a52739db0c7b280753e9
Nicholas Dujay <nickdujay@gmail.com>
2014-08-23 20:57:04 -0400

    Removed the vendor files, since main-bower-files handles it now. Changed sass so we don't have to import all the sass files now.

--- client/build.config.js
+++ client/build.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
     html: ['src/index.html'],
 
-    sass: 'src/app/**.scss'
+    sass: 'src/app/{,**/}*.scss'
   },
 
   testFiles: {
@@ -26,18 +26,5 @@ module.exports = {
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-scenario/angular-scenario.js'
     ]
-  },
-
-  vendorFiles: {
-    js: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-ui-router/release/angular-ui-router.js',
-      'bower_components/angular-touch/angular-touch.js',
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js'
-    ],
-    css: [],
-    assets: []
-  },
+  }
 };
